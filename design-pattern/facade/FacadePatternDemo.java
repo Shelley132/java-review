d5f4b2d12a60e4e3d8c407ef961a38eb57389cad
Shelley132 <liulijuan132@qq.com>
2017-08-06 19:26:07 +0800

    外观模式Demo

--- design-pattern/facade/FacadePatternDemo.java
+++ design-pattern/facade/FacadePatternDemo.java
@@ -3,7 +3,7 @@ package facade;
  * @author JUANJUAN
  * @version 2017年7月30日下午8:04:15
 */
-public class FacadeaPatternDemo {
+public class FacadePatternDemo {
 	public static void main(String[] args) {
 		ShapeMaker shapeMaker = new ShapeMaker();
 		shapeMaker.drawCircle();
